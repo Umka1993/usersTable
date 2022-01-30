d2a5ace712a3e4723c4588a6fef72a97abddce3a
Umka1993 <58582062+Umka1993@users.noreply.github.com>
2022-01-30 15:14:28 +0200

    Delete src directory

--- src/core/redux/hooks/redux.ts
+++
@@ -1,7 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "../store";
-import { AnyAction, ThunkDispatch } from "@reduxjs/toolkit";
-
-export const useAppDispatch = (): ThunkDispatch<RootState, void, AnyAction> =>
-  useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- src/core/redux/rootReducer.ts
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import { usersSlice } from "../../reduxModule/users/usersSlice";
-
-export const rootReducer = combineReducers({
-  users: usersSlice,
-});
